b9bb38e4a7411ab9c60104f46bfff6c9523a49d5
Colin Rodney <colin.rodney@ibm.com>
2022-09-30 18:27:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Button, Alert, Breadcrumb, Container} from "react-bootstrap"
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render(){
+      return(
+        <Container className="headliner d-lg-flex p-3">
+            <div className="mb-3">
+              <img className="demoImage" src="https://source.unsplash.com/8FdEwlxP3oU" alt="something"></img>
+            </div>
+            <div className="p-3 order-first">
+              <p classname="p-3">Lorem ipsum dolor sit amet</p>
+              <a href="#" classname="p-3">lorem ipsum </a>
+              <p classname="p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo donec enim diam vulputate ut pharetra.</p>
+              <Button className="call-to-action">Action</Button>
+            </div>
+          </Container>
+      )
+    }
 }
 
 export default App;
